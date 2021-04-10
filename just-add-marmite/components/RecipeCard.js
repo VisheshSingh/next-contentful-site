7df0e4e3a2887f0ba8a5c2268cd6153bd6b1653b
Vishesh Singh <VisheshSingh16@gmail.com>
2021-04-10 16:11:11 -0500

    Outputting Recipe Data

---
+++ just-add-marmite/components/RecipeCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+
+const RecipeCard = ({ recipe }) => {
+  const { title, slug, cookingTime, thumbnail } = recipe.fields;
+  return (
+    <div className='card'>
+      <div className='featured'>{/* img thumbnail */}</div>
+      <div className='content'>
+        <div className='info'>
+          <h4>{title}</h4>
+          <p>Takes about {cookingTime} mins to make</p>
+        </div>
+        <div className='actions'>
+          <Link href={`/recipes/${slug}`}>
+            <a>Cook this</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
